0d76bdbf9e525c1219f87c097451f4b736604472
vengad246 <vengadvengadesh2001@gmail..com>
2025-02-02 11:27:19 +0530

    vk

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+const cors = require("cors");
+server.use(cors());
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
