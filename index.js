ec2353d88d0f3f0ab250d0d8421ef031b1a19434
vengad246 <vengadvengadesh2001@gmail..com>
2025-02-02 12:08:07 +0530

    vk

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const cors = require("cors");
-server.use(cors());
+// const cors = require("cors");
+// server.use(cors());
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
